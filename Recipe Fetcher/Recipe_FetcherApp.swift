337f13738c585c3644eab31fd1a227964f829316
Joey Chung <jnchung@icloud.com>
2024-11-12 11:53:56 -0600

    Initial Commit

---
+++ Recipe Fetcher/Recipe_FetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_FetcherApp.swift
+//  Recipe Fetcher
+//
+//  Created by Joey Chung on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_FetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
